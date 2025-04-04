8c1dcb4a311746d5a36109a749fa6ae346d5702c
AiratMusin <musairat9@gmail.com>
2025-04-04 17:13:31 +0300

    Add files via upload

---
+++ VOT_hw3.py
@@ -0,0 +1,23 @@
+import sys
+sys.set_int_max_str_digits(0)
+# x = int(input())
+# while x % 2 == 1 and x % 5 != 0:
+#     x = int(input())
+
+# for i in range(10):
+#     print(i)
+
+# K = int(input())
+# N = int(input())
+# sum1 = 0
+# while K <= N:
+#     if K % 2 == 1:
+#         sum1 += K
+#     K += 1
+# print(sum1)
+
+# fact = int(input())
+# ans = 1
+# for i in range(fact):
+#     ans *= (i + 1)
+# print(ans)
