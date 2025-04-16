16023752d03ae05ac473d76a21cad0fd64c0fc2d
AiratMusin <musairat9@gmail.com>
2025-04-16 21:02:55 +0300

    Add files via upload

---
+++ VOT_hw7.py
@@ -0,0 +1,11 @@
+# print(sum([x**2 for x in range(101)]))
+
+# print(sum([1 for x in range(1, 21) if x % 2 == 0]))
+
+# n = int(input("Введите длину массива: "))
+# arr = []
+# for i in range(n):
+#     arr.append(int(input()))
+# print(sum([(x + 1) % 2 for x in arr[::2]]))
+
+# print(len([x for x in range(1, 1001) if (x % 7 == 0 or x % 11 == 0)]))s
